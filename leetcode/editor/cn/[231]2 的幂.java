0ff080d67c207d0e5c400f6c5dea7254a4b729f9
hong_chen <hong_chen@hunliji.com>
2021-08-19 08:43:46 +0800

    2 的幂 数学法

--- leetcode/editor/cn/[231]2 的幂.java
+++ leetcode/editor/cn/[231]2 的幂.java
@@ -56,22 +56,53 @@
 // 👍 393 👎 0
 
 //Java：2 的幂
-class PowerOfTwo{
+class PowerOfTwo {
     public static void main(String[] args) {
         Solution solution = new PowerOfTwo().new Solution();
 
-    System.out.println(Integer.toBinaryString(16));
-    System.out.println(Integer.toBinaryString(15));
-    System.out.println(16&15);
+        System.out.println(Integer.toBinaryString(16));
+        System.out.println(Integer.toBinaryString(15));
+        System.out.println(16 & 15);
         // TO TEST
     }
-    
+
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-      return n > 0 && (n & (n - 1)) == 0;
+    class Solution {
+//        /**
+//         * 方案一：位运算
+//         *
+//         * @param
+//         * @return
+//         */
+//        public boolean isPowerOfTwo(int n) {
+//            return n > 0 && (n & (n - 1)) == 0;
+//        }
+
+        /**
+         * 方案二：数学法
+         * @param
+         * @return
+         */
+        public boolean isPowerOfTwo(int n) {
+            if (n == 0) {
+                return false;
+            }
+            if (n == 1) {
+                return true;
+            }
+            while (n > 1) {
+                int remain = n % 2;
+                if (remain == 1) {
+                    return false;
+                }
+                n = n / 2;
+                if (n == 1) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
